0175ee15d35db5a5170aa8bd3d4a1431cdd810c0
Ken Payne <ken@meltano.com>
2023-01-26 11:12:35 +0000

    update standard tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,8 +4,7 @@ import uuid
 from pathlib import Path
 
 import pytest
-from singer_sdk.testing import TargetTestRunner, get_test_class, pytest_generate_tests
-from singer_sdk.testing.suites import target_tests
+from singer_sdk.testing import get_target_test_class
 
 from target_csv.target import TargetCSV
 
@@ -13,9 +12,8 @@ test_output_dir = Path(f".output/test_{uuid.uuid4()}/")
 
 SAMPLE_CONFIG = {"max_parallelism": 1, "output_path_prefix": f"{test_output_dir}/"}
 
-StandardTestsTargetCSV = get_test_class(
-    test_runner=TargetTestRunner(target_class=TargetCSV, config=SAMPLE_CONFIG),
-    test_suites=[target_tests],
+StandardTestsTargetCSV = get_target_test_class(
+    target_class=TargetCSV, config=SAMPLE_CONFIG
 )
 
 
