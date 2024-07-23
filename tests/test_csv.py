2a2f8dedd9e548c320492927fda1cbef7c7d04b1
Sebastian Smiley <ssmiley@autoidm.com>
2024-07-23 10:14:37 -0400

    get tests to fail

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -1,11 +1,15 @@
 """CSV-related tests."""
 
+import io
 from pathlib import Path
 from typing import Any, Dict, List, Tuple
+from contextlib import redirect_stdout
+import shutil
 
 import pytest
 
 from target_csv.serialization import read_csv, write_csv
+from target_csv.target import TargetCSV
 
 SAMPLE_DATASETS: List[Tuple[Dict, List[Dict[str, Any]]]] = [
     (
@@ -39,6 +43,26 @@ SAMPLE_DATASETS: List[Tuple[Dict, List[Dict[str, Any]]]] = [
     )
 ]
 
+def singer_file_to_target(file_name, target) -> None:
+    """Singer file to Target, emulates a tap run
+
+    Equivalent to running cat file_path | target-name --config config.json.
+    Note that this function loads all lines into memory, so it is
+    not good very large files.
+
+    Args:
+        file_name: name to file in .tests/data_files to be sent into target
+        Target: Target to pass data from file_path into..
+    """
+    file_path = Path(__file__).parent / Path("./data_files") / Path(file_name)
+    buf = io.StringIO()
+    with redirect_stdout(buf):
+        with open(file_path) as f:
+            for line in f:
+                print(line.rstrip("\r\n"))  # File endings are here,
+                # and print adds another line ending so we need to remove one.
+    buf.seek(0)
+    target.listen(buf)
 
 @pytest.fixture
 def output_dir() -> Path:
@@ -63,11 +87,8 @@ def test_file_paths(output_dir) -> List[Path]:
     paths = []
     for dir in range(4):
         path = Path(output_dir / f"test-dir-{dir}/csv-test-output-{dir}.csv")
-        if path.exists():
-            path.unlink()
-
+        path.unlink(missing_ok=True)
         paths.append(path)
-
     return paths
 
 
@@ -90,3 +111,17 @@ def test_csv_roundtrip(output_filepath) -> None:
             for key in orig_record.keys():
                 # Note: Results are stringified during serialization
                 assert str(orig_record[key]) == new_record[key]
+
+def test_csv_multiple_streams(output_dir) -> None:
+    """Tests syncing multiple streams. 30 times in a row to catch race condition.
+    
+    Further discussion in #129.
+    """
+    file_name = "users_and_employees.singer"
+    for i in range(30):
+        path = Path(output_dir / "test_csv_multiple_streams" / f"{i}")
+        shutil.rmtree(path, ignore_errors=True)
+        singer_file_to_target(
+            file_name=file_name,
+            target=TargetCSV(config={"output_path": str(path)})
+        )
