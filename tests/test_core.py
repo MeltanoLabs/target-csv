cbbf56dbe8173cc33df907fdc883e54eb9c278b8
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-07-24 10:01:37 -0400

    Use a custom test suite

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,11 @@
 """Tests standard target features using the built-in SDK tests library."""
 
+from pathlib import Path
 from typing import Any, Dict
 
 from singer_sdk.testing import get_target_test_class
+from singer_sdk.testing.suites import TestSuite
+from singer_sdk.testing.templates import TargetFileTestTemplate
 
 from target_csv.target import TargetCSV
 
@@ -10,4 +13,23 @@ SAMPLE_CONFIG: Dict[str, Any] = {
     "escape_character": '"',
 }
 
-TestTargetCSV = get_target_test_class(TargetCSV, config=SAMPLE_CONFIG)
+
+class MultipleStreamsTest(TargetFileTestTemplate):
+    name = "users_and_employees"
+
+    @property
+    def singer_filepath(self) -> Path:
+        current_dir = Path(__file__).resolve().parent
+        return current_dir / "data_files" / f"{self.name}.singer"
+
+
+TestTargetCSV = get_target_test_class(
+    TargetCSV,
+    config=SAMPLE_CONFIG,
+    custom_suites=[
+        TestSuite(
+            kind="target",
+            tests=[MultipleStreamsTest],
+        )
+    ],
+)
