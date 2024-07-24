d2a81a6c009e96fa58b4d0cca328bb0face86be0
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-07-24 10:08:53 -0400

    Use `importlib.resources`

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,13 +1,20 @@
 """Tests standard target features using the built-in SDK tests library."""
 
 from pathlib import Path
+import sys
 from typing import Any, Dict
 
 from singer_sdk.testing import get_target_test_class
 from singer_sdk.testing.suites import TestSuite
 from singer_sdk.testing.templates import TargetFileTestTemplate
 
+from . import data_files
 from target_csv.target import TargetCSV
+
+if sys.version_info >= (3, 9):
+    from importlib.resources import files
+else:
+    from importlib_resources import files
 
 SAMPLE_CONFIG: Dict[str, Any] = {
     "escape_character": '"',
@@ -19,8 +26,7 @@ class MultipleStreamsTest(TargetFileTestTemplate):
 
     @property
     def singer_filepath(self) -> Path:
-        current_dir = Path(__file__).resolve().parent
-        return current_dir / "data_files" / f"{self.name}.singer"
+        return files(data_files) / f"{self.name}.singer"
 
 
 TestTargetCSV = get_target_test_class(
